e70ccb7dcc0f43dee64026f4858566c03204294e
lucas.f.lacerda@usp.br <seuemail@exemplo.com>
2026-06-10 14:43:55 -0300

    viewport para mobile

--- Experimento4/webserver_led/webserver_led.ino
+++ Experimento4/webserver_led/webserver_led.ino
@@ -40,6 +40,7 @@ String paginaHTML() {
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PWM LED ESP32-C3</title>
     <style>
         body {
--- Experimento4/webserver_servo/webserver_servo.ino
+++ Experimento4/webserver_servo/webserver_servo.ino
@@ -43,6 +43,7 @@ String paginaHTML() {
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PWM Servo ESP32-C3</title>
     <style>
         body {
