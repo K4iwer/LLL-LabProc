9e0627fa77db53adab3a501ba5661c40d6d9f127
lucas.f.lacerda@usp.br <seuemail@exemplo.com>
2026-07-01 15:11:33 -0300

    fix: usando biblioteca do teclado

--- Experimento6/perifericos/teclado/keypad.c
+++ Experimento6/perifericos/teclado/keypad.c
@@ -1,77 +1,36 @@
-#include "keypad.h"
-
-#include <wiringPi.h>
-
-#define NUM_ROWS 4
-#define NUM_COLS 4
-
-/* Numeração WiringPi */
-static const int rows[NUM_ROWS] =
-{
-    0, 2, 3, 4
+/**********************************************************************
+* Filename    : MatrixKeypad.cpp
+* Description : Obtain the key code of 4x4 Matrix Keypad
+* Author      : www.freenove.com
+* modification: 2024/07/29
+**********************************************************************/
+#include "Keypad.hpp"
+#include <stdio.h>
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+char keys[ROWS][COLS] = {  //key code
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
+byte rowPins[ROWS] = {16, 20, 21, 26}; //define the row pins for the keypad
+byte colPins[COLS] = {19, 13, 6, 5};   //define the column pins for the keypad
+//create Keypad object
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-static const int cols[NUM_COLS] =
-{
-    5, 6, 21, 22
-};
-
-static const char keys[4][4] =
-{
-    {'1','2','3','A'},
-    {'4','5','6','B'},
-    {'7','8','9','C'},
-    {'*','0','#','D'}
-};
-
-bool keypad_init(void)
-{
-    if (wiringPiSetup() == -1)
-        return false;
-
-    for (int i = 0; i < NUM_ROWS; i++)
-    {
-        pinMode(rows[i], OUTPUT);
-        digitalWrite(rows[i], LOW);
-    }
-
-    for (int i = 0; i < NUM_COLS; i++)
-    {
-        pinMode(cols[i], INPUT);
-        pullUpDnControl(cols[i], PUD_DOWN);
-    }
-
-    return true;
-}
-
-char keypad_getKey(void)
-{
-    while (1)
-    {
-        for (int r = 0; r < NUM_ROWS; r++)
-        {
-            for (int i = 0; i < NUM_ROWS; i++)
-                digitalWrite(rows[i], LOW);
-
-            digitalWrite(rows[r], HIGH);
-
-            delay(1);
-
-            for (int c = 0; c < NUM_COLS; c++)
-            {
-                if (digitalRead(cols[c]))
-                {
-                    while (digitalRead(cols[c]));
-
-                    delay(20);
-
-                    return keys[r][c];
-                }
-            }
+int main(){
+    printf("Program is starting ... \n");
+    
+    wiringPiSetupGpio();//Initialize wiringPi. Use BCM Number.
+    
+	char key = 0;
+	keypad.setDebounceTime(50);
+    while(1){
+        key = keypad.getKey();  //get the state of keys
+        if (key){       //if a key is pressed, print out its key code
+            printf("You Pressed key :  %c \n",key);
         }
     }
-}
-
-void keypad_close(void)
-{
+    return 1;
 }
